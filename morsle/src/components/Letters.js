f4c939b036da153ceff690ad9d9f97497bcc510b
Jonathan Pierre-Louis <jopierrelouis@gmail.com>
2023-02-23 18:41:25 -0500

    broken, needs to be fixed

---
+++ morsle/src/components/Letters.js
@@ -0,0 +1,17 @@
+import Letter from "./Letter"
+
+const Letters = ({wordOfTheDay, inputArray, count}) => {
+    inputArray = inputArray + '';
+    const input = inputArray.split('');
+  return (
+    <div>
+        <Letter expected={`${wordOfTheDay[0]}`} actual={`${inputArray[0]}`} arr={wordOfTheDay}></Letter>
+        <Letter expected={`${wordOfTheDay[1]}`} actual={`${inputArray[1]}`} arr={wordOfTheDay}></Letter>
+        <Letter expected={`${wordOfTheDay[2]}`} actual={`${inputArray[2]}`} arr={wordOfTheDay}></Letter>
+        <Letter expected={`${wordOfTheDay[3]}`} actual={`${inputArray[3]}`} arr={wordOfTheDay}></Letter>
+        <Letter expected={`${wordOfTheDay[4]}`} actual={`${inputArray[4]}`} arr={wordOfTheDay}></Letter>
+    </div>
+  )
+}
+
+export default Letters
--- morsle/src/components/Main.js
+++ morsle/src/components/Main.js
@@ -1,43 +1,45 @@
 import { useState, useEffect } from "react";
-import Letter from "./Letter";
+import Letters from "./Letters";
 
 const Main = () => {
   const wordOfTheDay = ['B','R','E','A','D'];
   const [word, setWord] = useState('');    
-  const [inputArray, setInputArray] = useState([]); 
+  const [inputArray, setInputArray] = useState([]);
+  let inputArr = ["","","","",""];
+  const [wordAttempt, setWordAttemp] = useState(0);
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    
-    setInputArray(word.split(''));
+    inputArr[wordAttempt] = word;
+    setInputArray(inputArr);
+    setWordAttemp(wordAttempt + 1);
 
     setWord('');
   }
 
   useEffect(() => {
     
+    // console.log(word)
     console.log(inputArray)
     
   }, [inputArray])
 
   return (
     <>
-      {/* Submission form */}
       <div className="container">
+        {/* Submission form */}
         <form onSubmit={onSubmit}>
           <input type="text" placeholder="Enter Word Here" minLength={5} maxLength={5} onChange={(e) => setWord(e.target.value.toUpperCase())}/>
           <input type="submit" />
         </form>
 
         {/* div showing list of letters and if they are corret or not */}
-        <div>
-          <Letter expected={`${wordOfTheDay[0]}`} actual={`${inputArray[0]}`} arr={wordOfTheDay}></Letter>
-          <Letter expected={`${wordOfTheDay[1]}`} actual={`${inputArray[1]}`} arr={wordOfTheDay}></Letter>
-          <Letter expected={`${wordOfTheDay[2]}`} actual={`${inputArray[2]}`} arr={wordOfTheDay}></Letter>
-          <Letter expected={`${wordOfTheDay[3]}`} actual={`${inputArray[3]}`} arr={wordOfTheDay}></Letter>
-          <Letter expected={`${wordOfTheDay[4]}`} actual={`${inputArray[4]}`} arr={wordOfTheDay}></Letter>
-        </div>
+        {wordAttempt >= 0 && <Letters wordOfTheDay={wordOfTheDay} inputArray={inputArray[0]}></Letters>}
+        {wordAttempt >= 1 && <Letters wordOfTheDay={wordOfTheDay} inputArray={inputArray[1]}></Letters>}
+        {wordAttempt >= 2 && <Letters wordOfTheDay={wordOfTheDay} inputArray={inputArray[2]}></Letters>}
+        {wordAttempt >= 3 && <Letters wordOfTheDay={wordOfTheDay} inputArray={inputArray[3]}></Letters>}
+        {wordAttempt >= 4 && <Letters wordOfTheDay={wordOfTheDay} inputArray={inputArray[4]}></Letters>}
       </div>
     </>
   )
